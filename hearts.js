4dc07fa4d3184942b447263709010abd11e4446a
Yllishedrine <ty202502680@wmsu.edu.ph>
2026-07-05 19:53:14 +0800

    Update hearts.js

--- hearts.js
+++ hearts.js
@@ -1,1 +1,25 @@
+function createHeart(){
 
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random()*100 + "vw";
+
+    heart.style.fontSize = (15 + Math.random()*30) + "px";
+
+    heart.style.animationDuration = (4 + Math.random()*4) + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+
+        heart.remove();
+
+    },8000);
+
+}
+
+setInterval(createHeart,350);
