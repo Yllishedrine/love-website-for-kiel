9f88e17d9ae0f59acc7bd211710e34c092054784
Yllishedrine <ty202502680@wmsu.edu.ph>
2026-07-05 20:36:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const startDate = new Date("2025-05-28T00:00:00");
-
+const startDate = new Date("2026-05-28T00:00:00");
 function updateCounter() {
     const counter = document.getElementById("counter");
     if (!counter) return;
