37b5d304460c53fbbaaa9c35472287c84c766e7d
Yllishedrine <ty202502680@wmsu.edu.ph>
2026-07-06 16:13:47 +0800

    Update surprise.js

--- surprise.js
+++ surprise.js
@@ -1,1 +1,32 @@
+const messages = [
 
+"I love you so much.",
+
+"Thank you for always making me smile.",
+
+"You're my safe place.",
+
+"I hope we stay together forever.",
+
+"Every day with you is my favorite.",
+
+"You're the best thing that's ever happened to me.",
+
+"Thank you for choosing me.",
+
+"I'll always be here for you."
+
+];
+
+function showMessage() {
+document.getElementById("coverText").style.display = "none";
+document.getElementById("hiddenMessage").classList.add("show");
+}
+
+document.getElementById("btn").addEventListener("click", () => {
+
+const random = Math.floor(Math.random()*messages.length);  
+
+document.getElementById("message").innerHTML = messages[random];
+
+});
